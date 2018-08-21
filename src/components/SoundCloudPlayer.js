d4e02c9038376df72b83ecb7c6b5a1dd0260f5d1
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-21 12:14:26 +0200

    adding soundplayer comp v0.04

---
+++ src/components/SoundCloudPlayer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { withSoundCloudAudio } from 'react-soundplayer/addons';
+import {
+  PlayButton,
+  PrevButton,
+  NextButton,
+  Progress,
+  Timer,
+  VolumeControl
+} from 'react-soundplayer/components';
+
+export default class SoundCoudPlayer extends React.Component {
+
+
+}
