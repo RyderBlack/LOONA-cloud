d37c10e7b76d27e95e4a8e6d7eb12742225d5848
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-23 15:46:58 +0200

    api key secured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import SoundcloudPlayer from './components/SoundCloudPlayer.js';
+import VideoPlayer from './components/VideoPlayer.js';
 import './App.css';
 
+//console.log(process.env.REACT_APP_APKY);
+const KEYT =`${process.env.REACT_APP_APKY}`
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoList : []
+    }
+  }
+
+  componentWillMount() {
+   fetch(`https://www.googleapis.com/youtube/v3/search?key=${KEYT}&channelId=UCOJplhB0wGQWv9OuRmMT-4g&part=snippet,id&order=date&maxResults=20.json`)
+      .then(function (data) {
+        
+        
+        const videos = data;
+        console.log(videos);
+        return videos;
+      }).catch(error => {
+        console.log(error)
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <SoundcloudPlayer />
+        <VideoPlayer />
       </div>
     );
   }
---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class VideoPlayer extends React.Component {
+
+  render() {
+    return (
+      <p>Video</p>
+    )
+  }
+
+}
