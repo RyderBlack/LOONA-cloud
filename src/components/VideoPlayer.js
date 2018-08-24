8faf72d97e41af7d00db101bcbaa15d7ea98c5b1
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-24 17:09:23 +0200

    adding data thumbnails

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -8,8 +8,14 @@ export default class VideoPlayer extends Component {
       <Fragment>
       <p>Video title = </p>
       {this.props.videos.map((vid) => {
+        //console.log(vid.snippet.resourceId.videoId);
         return (
-          <li key={vid.id.videoId}>{vid.snippet.title}</li>
+          //<iframe key={vid.id.videoId} width="560" height="315" src={`https://www.youtube.com/embed/${vid.resourceId.videoId}`} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+          <div key={vid.snippet.resourceId.videoId}>
+          <img src={ vid.snippet.thumbnails.maxres ? `http://img.youtube.com/vi/${vid.snippet.resourceId.videoId}/maxresdefault.jpg` : `http://img.youtube.com/vi/${vid.snippet.resourceId.videoId}/hqdefault.jpg` } 
+          alt={vid.snippet.title} />
+            <p>{vid.snippet.title}</p>
+          </div>
         )
       })
       }
