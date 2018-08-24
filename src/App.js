2980b00824c3d8c1d5a700c48540ffbff575e262
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-24 15:10:09 +0200

    display data success

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <VideoPlayer />
+        <VideoPlayer videos={this.state.videoList}/>
       </div>
     );
   }
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component, Fragment} from 'react';
 
 
-export default class VideoPlayer extends React.Component {
+export default class VideoPlayer extends Component {
 
   render() {
     return (
-      <p>Video</p>
+      <Fragment>
+      <p>Video title = </p>
+      {this.props.videos.map((vid) => {
+        return (
+          <li key={vid.id.videoId}>{vid.snippet.title}</li>
+        )
+      })
+      }
+      </Fragment>
     )
   }
 
